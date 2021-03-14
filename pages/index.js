9b73d5e30b4feedc8db8ba0b44aa6a237830e42f
Natália Gil <nataliadasilva.dev@gmail.com>
2021-03-14 13:26:32 -0300

    feat: estilizando a página de login

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/login.module.css'
 
 import {
   Card, CardBody, CardFooter, Form, FormGroup, Input, Button, CardHeader, Container
@@ -10,30 +10,32 @@ import {
 export default function Home() {
   const router = useRouter();
   return (
-    <Container>
-      <main className={styles.main}>
-        <h1>DoTask</h1>
-        <Card>
-          <CardHeader>Login</CardHeader>
-          <CardBody>
-            <Form>
-              <FormGroup>
-                <Input placeholder="Email" />
-              </FormGroup>
-              <FormGroup>
-                <Input placeholder="Senha" />
-              </FormGroup>
+    <div className={styles.background}>
+      <Container>
+        <main className={styles.main}>
+          <h1>DoTask</h1>
+          <Card className={styles.login_card}>
+            <CardHeader className={styles.card_header}>Login</CardHeader>
+            <CardBody>
+              <Form>
+                <FormGroup>
+                  <Input className={styles.input} placeholder="Email" />
+                </FormGroup>
+                <FormGroup>
+                  <Input className={styles.input} placeholder="Senha" />
+                </FormGroup>
 
-              <Button onClick={ () => router.push('/list') } color='primary'>Entrar</Button>
-            </Form>
-          </CardBody>
+                <Button className={styles.button} onClick={() => router.push('/list')} color='primary'>Entrar</Button>
+              </Form>
+            </CardBody>
 
-          <CardFooter>
-            <span>Ainda não tem uma conta?</span>
-            <Link href="/register">Cadastre-se</Link>
-          </CardFooter>
-        </Card>
-      </main>
-    </Container>
+            <CardFooter className={styles.card_footer}>
+              <span>Ainda não tem uma conta?</span>
+              <Link href="/register">Cadastre-se</Link>
+            </CardFooter>
+          </Card>
+        </main>
+      </Container>
+    </div>
   )
 }
--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/login.module.css'
 import UserService from '../services/user'
 import {
   Card, CardBody, CardFooter, Form, FormGroup, Input, Button, CardHeader, Container
@@ -8,18 +8,18 @@ import {
 import { useState } from 'react';
 
 export default function Register() {
-  const [ name, setName ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ confirmPassword, setConfirmPassword] = useState('');
-  const insertUser = () =>{    
-    if ( password !== confirmPassword ) {
-      console.log('Senhas não são iguais');  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const insertUser = () => {
+    if (password !== confirmPassword) {
+      console.log('Senhas não são iguais');
     } else {
-      UserService.create({ name, email, password }).then(result =>{
+      UserService.create({ name, email, password }).then(result => {
         alert(name, 'foi cadastrado')
       })
-    }    
+    }
   }
 
   return (
@@ -31,19 +31,19 @@ export default function Register() {
           <CardBody>
             <Form>
               <FormGroup>
-                <Input placeholder="Nome" type='text' value={name} onChange={(e)=> setName(e.target.value)} />
+                <Input placeholder="Nome" type='text' value={name} onChange={(e) => setName(e.target.value)} />
               </FormGroup>
               <FormGroup>
-                <Input placeholder="E-mail" type='text' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <Input placeholder="E-mail" type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
               </FormGroup>
               <FormGroup>
-                <Input placeholder="Senha" type='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                <Input placeholder="Senha" type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
               </FormGroup>
               <FormGroup>
-                <Input placeholder="Repetir Senha" type='password' value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
+                <Input placeholder="Repetir Senha" type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
               </FormGroup>
 
-              <Button color='primary' onClick={()=> insertUser()} >Cadastrar</Button>
+              <Button color='primary' onClick={() => insertUser()} >Cadastrar</Button>
             </Form>
           </CardBody>
 
