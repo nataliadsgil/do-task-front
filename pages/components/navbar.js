1305e2f84448dd78b1564ae9b4158e58d1bb4c48
Daniel Moya da Silva <danielm.silva@univem.edu.br>
2021-04-23 19:46:25 -0300

    feat: Tela de perfil

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,19 +1,22 @@
 import {
-  Navbar, NavbarBrand, NavItem, NavLink, Nav
+  Navbar, NavbarBrand, NavItem, Button, Nav
 } from 'reactstrap'
+import { useRouter } from 'next/router';
 import { userName } from '../../services/user'
 import styles from '../../styles/navbar.module.css'
 
 export default function Home() {
+  const router = useRouter();
   const getInitials = () => {
     return userName.split(" ").map((n) => n[0]).join("");
   }
+
   return (
     <Navbar className={styles.mainNavbar}>
       <NavbarBrand>DoTask</NavbarBrand>
       <Nav>
         <NavItem>
-          <NavLink>{userName}</NavLink>
+          <Button className={styles.button_perfil} onClick={() => router.push('/perfil')} >{userName}</Button>
         </NavItem>
       </Nav>
     </Navbar>
